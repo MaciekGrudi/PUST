0de486eede44f37f7ba89b44a97c7097ecb246f0
MaciekGrudi <grumaciek@gmail.com>
2018-03-23 20:44:40 +0100

    PLOTS LAB

--- PROJEKT 1/LAB/LAB1/PLOTS.m
+++ PROJEKT 1/LAB/LAB1/PLOTS.m
@@ -92,78 +92,87 @@ clear Y U Yzad E_wsk;
 
 load DMC_400_200_1_0;
 
-E_wsk=0;
+Y_1_0=Y;
+U_1_0=U;
+
+E_wsk_1_0=0;
 for i=1:length(Y)
-    E_wsk=E_wsk+(Yzad(i)-Y(i))^2;
+    E_wsk_1_0=E_wsk_1_0+(Yzad(i)-Y(i))^2;
 end
 
-figure(5);
-stairs(Y,'b');
-hold on;
-stairs(Yzad,'r');
-xlabel('k');
-ylabel('Y');
-title(strcat('DMC, lambda=1.0, E=',num2str(E_wsk)));
-legend('Y(k)','Yzad(k)');
-grid on;
-axis([0 400 24 31]);
+% figure(5);
+% stairs(Y,'b');
+% hold on;
+% stairs(Yzad,'r');
+% xlabel('k');
+% ylabel('Y');
+% title(strcat('DMC, lambda=1.0, E=',num2str(E_wsk)));
+% legend('Y(k)','Yzad(k)');
+% grid on;
+% axis([0 400 24 31]);
+% 
+% if save_==1
+%     print('PLOTS_DIR/DMC_400_200_1_0_Y','-dpng','-r500');
+%     matlab2tikz('PLOTS_dir/DMC_400_200_1_0_Y.tex','showInfo',false);
+% end
+% 
+% figure(6);
+% stairs(U,'b');
+% xlabel('k');
+% ylabel('U');
+% title('DMC, lambda=1.0');
+% legend('U(k)');
+% grid on;
+% axis([0 400 0 70]);
+% 
+% if save_==1
+%     print('PLOTS_DIR/DMC_400_200_1_0_U','-dpng','-r500');
+%     matlab2tikz('PLOTS_dir/DMC_400_200_1_0_U.tex','showInfo',false);
+% end
 
-if save_==1
-    print('PLOTS_DIR/DMC_400_200_1_0_Y','-dpng','-r500');
-    matlab2tikz('PLOTS_dir/DMC_400_200_1_0_Y.tex','showInfo',false);
-end
-
-figure(6);
-stairs(U,'b');
-xlabel('k');
-ylabel('U');
-title('DMC, lambda=1.0');
-legend('U(k)');
-grid on;
-axis([0 400 0 70]);
-
-if save_==1
-    print('PLOTS_DIR/DMC_400_200_1_0_U','-dpng','-r500');
-    matlab2tikz('PLOTS_dir/DMC_400_200_1_0_U.tex','showInfo',false);
-end
-
-clear Y Yzad U E_wsk;
+clear Y Yzad U;
 
 load DMC_400_200_0_1;
 
-E_wsk=0;
+Y_0_1=Y;
+U_0_1=U;
+
+E_wsk_0_1=0;
 for i=1:length(Y)
-    E_wsk=E_wsk+(Yzad(i)-Y(i))^2;
+    E_wsk_0_1=E_wsk_0_1+(Yzad(i)-Y(i))^2;
 end
 
-figure(7);
-stairs(Y,'b');
+figure(5);
+stairs(Y_1_0,'b');
 hold on;
+stairs(Y_0_1,'g');
 stairs(Yzad,'r');
 xlabel('k');
 ylabel('Y');
-title(strcat('DMC, lambda=0.1, E=',num2str(E_wsk)));
-legend('Y(k)','Yzad(k)');
+title('Regulator DMC, Y(k)');
+legend('lambda=1.0','lambda=0.1','Yzad');
 grid on;
 axis([0 400 24 31]);
 
 if save_==1
-    print('PLOTS_DIR/DMC_400_200_0_1_Y','-dpng','-r500');
-    matlab2tikz('PLOTS_dir/DMC_400_200_0_1_Y.tex','showInfo',false);
+    print('PLOTS_DIR/DMC_Y','-dpng','-r500');
+    matlab2tikz('PLOTS_dir/DMC_Y.tex','showInfo',false);
 end
 
-figure(8);
-stairs(U,'b');
+figure(6);
+stairs(U_1_0,'b');
+hold on;
+stairs(U_0_1,'g');
 xlabel('k');
 ylabel('U');
-title('DMC, lambda=0.1');
-legend('U(k)');
+title('Regulator DMC, U(k)');
+legend('lambda=1.0','lambda=0.1');
 grid on;
 axis([0 400 0 70]);
 
 if save_==1
-    print('PLOTS_DIR/DMC_400_200_0_1_U','-dpng','-r500');
-    matlab2tikz('PLOTS_dir/DMC_400_200_0_1_U.tex','showInfo',false);
+    print('PLOTS_DIR/DMC_U','-dpng','-r500');
+    matlab2tikz('PLOTS_dir/DMC_U.tex','showInfo',false);
 end
 
 clear Y Yzad U E_wsk;
