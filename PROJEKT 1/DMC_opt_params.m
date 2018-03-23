95dee66411378c8d2bfece661f6cc6de5870e980
MaciekGrudi <grumaciek@gmail.com>
2018-03-23 20:16:35 +0100

    Merge branch 'master' of https://github.com/MaciekGrudi/PUST

--- PROJEKT 1/DMC_opt_params.m
+++ PROJEKT 1/DMC_opt_params.m
@@ -1,3 +1,5 @@
+<<<<<<< HEAD
+<<<<<<< HEAD
 IntCon = 1:2;
 X0=[200, 15, 1];
 A=[]; B=[]; Aeq=[]; Beq=[];
@@ -20,4 +22,52 @@ stairs(Yzad,'--r');
 hold on;
 stairs(Y, 'b')
 ylabel('Y, Yzad');
+=======
+IntCon = 1:2;
+X0=[200, 15, 1];
+A=[]; B=[]; Aeq=[]; Beq=[];
+LB=[1, 1, 1]; UB=[1500, 1500, 100]; Nonlcon = [];
+X = ga(@DMC_optimization,3,A,B,Aeq,Beq,LB,UB,Nonlcon,IntCon);
+
+Upp = 0.8;
+
+[Y,U,Yzad,E] = DMC_simulation(X,Upp);
+
+figure;
+subplot(2,1,1);
+stairs(U,'b');
+ylabel('U');
+xlabel('k');
+title(sprintf('DMC, E=%e',E));
+
+subplot(2,1,2);
+stairs(Yzad,'--r');
+hold on;
+stairs(Y, 'b')
+ylabel('Y, Yzad');
+>>>>>>> 1aa569faa9f60a46403ca971df9c995fe9adabf0
+=======
+IntCon = 1:2;
+X0=[200, 15, 1];
+A=[]; B=[]; Aeq=[]; Beq=[];
+LB=[1, 1, 1]; UB=[1500, 1500, 100]; Nonlcon = [];
+X = ga(@DMC_optimization,3,A,B,Aeq,Beq,LB,UB,Nonlcon,IntCon);
+
+Upp = 0.8;
+
+[Y,U,Yzad,E] = DMC_simulation(X,Upp);
+
+figure;
+subplot(2,1,1);
+stairs(U,'b');
+ylabel('U');
+xlabel('k');
+title(sprintf('DMC, E=%e',E));
+
+subplot(2,1,2);
+stairs(Yzad,'--r');
+hold on;
+stairs(Y, 'b')
+ylabel('Y, Yzad');
+>>>>>>> 1aa569faa9f60a46403ca971df9c995fe9adabf0
 xlabel('k');
