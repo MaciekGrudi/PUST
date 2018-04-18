213fa0fa385723c68adf1f844764b0191a6477d4
Stanibor <stani.bork@gmail.com>
2018-04-18 21:10:59 +0200

    mała poprawka

--- PROJEKT 2/PROJEKT/symulacja_obiektu_UppYppZpp.m
+++ PROJEKT 2/PROJEKT/symulacja_obiektu_UppYppZpp.m
@@ -10,20 +10,20 @@ function Y=symulacja_obiektu_UppYppZpp(Upp,Ypp,Zpp,time,t)
     Y=zeros(time,1);
     Z=zeros(time,1);
     if (t == 1)
-        for k=16:time
+        for k=16:time+16
             U(k)=Upp;
             Z(k)=Zpp;
             Y(k)=symulacja_obiektu7y(U(k-4),U(k-5),Z(k-1),Z(k-2),Y(k-1),Y(k-2));
         end
-        Y=Y(16:time,1);
+        Y=Y(16:time+16,1);
     end
     
     if (t == 2)
-        for k=16:time
+        for k=16:time+16
             U(k)=Upp;
             Z(k)=Zpp;
             Y(k)=symulacja_obiektu7y(0,0,Z(k-1),Z(k-2),Y(k-1),Y(k-2));
         end
-        Y=Y(16:time,1);
+        Y=Y(16:time+16,1);
     end
 end
